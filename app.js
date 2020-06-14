5949e997461071d552aa638259a6ec8e2ebbe5af
huntdavid175 <huntdavid175@gmail.com>
2020-06-14 12:00:54 +0100

    added header and set body image

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("working!!")
+const title = document.querySelector("#title").value;
+const author = document.querySelector("#author").value
+// console.log(title, author)
+
+
+
+
+// Create a Constructor for the book objects 
+
+function Book(title,author,pages,status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = status;
+}
+
+let book = new Book(title,author)
+console.log(book)
+
