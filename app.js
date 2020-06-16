bdd5d7774934d454bfdc0153ad946ea02420cfa4
huntdavid175 <huntdavid175@gmail.com>
2020-06-16 14:53:49 +0100

    added delete functionality

--- app.js
+++ app.js
@@ -1,142 +1,176 @@
 const openForm = document.querySelector(".main-button");
 const modal = document.querySelector(".modal-container");
 const closeModal = document.querySelector(".close");
-const inputs = document.querySelectorAll("input");
-const submitBtn = document.querySelector(".secondary-button")
+const inputs = document.querySelectorAll(".form-input");
+const submitBtn = document.querySelector(".secondary-button");
 const toaster = document.querySelector("#toaster");
-const cardSection = document.querySelector(".cards-section")
-
+const cardSection = document.querySelector(".cards-section");
 
 // Change input fields border color when null and when not null
 
-inputs.forEach(function(input){
-    input.addEventListener("click",function(e){
-        e.target.style.border = "1px solid green";
-    })
+inputs.forEach(function (input) {
+	input.addEventListener("click", function (e) {
+		e.target.style.border = "1px solid green";
+	});
 });
 
-inputs.forEach(function(input){
-    input.addEventListener("keyup",function(e){
-        if (e.target.value.trim() == ""){
-            e.target.style.border = "1px solid red"
-        }
-        else{
-            e.target.style.border ="1px solid green"
-        }
-    })
-})
-
-
-// Open Modal functionality 
-
-openForm.addEventListener("click",function(e){
-    modal.style.display = "flex"
-    clearInputs()
-})
-
-// CLose Modal functionality 
-
-closeModal.addEventListener("click", function(){
-    modal.style.display = "none"
-})
-
-// Submit functionality 
-
-submitBtn.addEventListener("click",function(e){
-    if(checkIfInputsEmpty() == true){
-        addBook()
-        clearInputs()
-    }
+inputs.forEach(function (input) {
+	input.addEventListener("keyup", function (e) {
+		if (e.target.value.trim() == "") {
+			e.target.style.border = "1px solid red";
+		} else {
+			e.target.style.border = "1px solid green";
+		}
+	});
 });
 
+// Open Modal functionality
 
-function checkIfInputsEmpty(){
-    let val = 0
-    inputs.forEach(function(input){
-        if(input.value != ""){
-            val += 1
-        }
-    })
+openForm.addEventListener("click", function (e) {
+	modal.style.display = "flex";
+	clearInputs();
+});
 
-    if (val == 0){
-        inputs.forEach(function(input){
-            input.style.borderColor = "red"
-        })
-    }
-    if (val == 3){
-        toaster.textContent = "Book Added"
-        toaster.style.backgroundColor = "#4a4";
-        toaster.style.color = "#ffffff"
-        toaster.className = "show"
-        setTimeout(function(){
-            toaster.classList.remove("show")
-        },3000)
-        return true
-    }
-    else {
-        toaster.textContent = "Please fill all fields";
-        toaster.style.backgroundColor = "#f2dede";
-        toaster.style.color = "#a94442"
-        toaster.className = "show";
-        setTimeout(function(){
-            toaster.classList.remove("show")
-        },3000)
-    }
+// CLose Modal functionality
+
+closeModal.addEventListener("click", function () {
+	modal.style.display = "none";
+});
+
+// Submit functionality
+
+submitBtn.addEventListener("click", function (e) {
+	if (checkIfInputsEmpty() == true) {
+		addBook();
+		clearInputs();
+	}
+});
+
+function checkIfInputsEmpty() {
+	let val = 0;
+	inputs.forEach(function (input) {
+		if (input.value != "") {
+			val += 1;
+		}
+	});
+
+	if (val == 0) {
+		inputs.forEach(function (input) {
+			input.style.borderColor = "red";
+		});
+	}
+	if (val == 3) {
+		toaster.textContent = "Book Added";
+		toaster.style.backgroundColor = "#4a4";
+		toaster.style.color = "#ffffff";
+		toaster.className = "show";
+		setTimeout(function () {
+			toaster.classList.remove("show");
+		}, 3000);
+		return true;
+	} else {
+		toaster.textContent = "Please fill all fields";
+		toaster.style.backgroundColor = "#f2dede";
+		toaster.style.color = "#a94442";
+		toaster.className = "show";
+		setTimeout(function () {
+			toaster.classList.remove("show");
+		}, 3000);
+	}
 }
 
-// Clear inputs function 
+// Clear inputs function
 
-function clearInputs(){
-    inputs.forEach(function(input){
-        input.value = ""
-        input.style.borderColor = "gray"
-    })
+function clearInputs() {
+	inputs.forEach(function (input) {
+		input.value = "";
+		input.style.borderColor = "gray";
+	});
 }
 
-// Constructor for books 
+// Constructor for books
 
-function Book(title, author, pages, status){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = status;
+function Book(title, author, pages, status) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.isRead = status;
 }
 
-// Collect Input Fields 
-let bookArray = []
-function addBook(){
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const status = document.querySelector("#read").checked
+Book.prototype.updateStatus = function (state) {
+	return (this.isRead = state);
+};
 
-    let book = new Book(title,author,pages,status);
-    bookArray.push(book)
-    showBook()
+// Collect Input Fields
+let bookArray = [];
+function addBook() {
+	const title = document.querySelector("#title").value;
+	const author = document.querySelector("#author").value;
+	const pages = document.querySelector("#pages").value;
+	const status = document.querySelector("#read").checked;
+
+	let book = new Book(title, author, pages, status);
+	bookArray.push(book);
+	showBook();
 }
 
-// Add books to page 
+// Add books to page
 
-function showBook(){
-   cardSection.innerHTML = `${bookArray.map(function(book){
-       return `
-       <div class="card">
+function showBook() {
+	cardSection.innerHTML = `${bookArray
+		.map(function (book,i) {
+			return `
+       <div class="card" data-book-index= "${i}">
                <h2 id="card-title">${book.title}</h2>
                <h4 id="card-author">${book.author}</h4>
                <p id="card-pages">${book.pages} pages</p>
                <span id="card-status">${isRead(book)}</span>
+               <input type="checkbox" class="read-update" name="read" value="Read">
                <button class="delete-book">Delete</button>
            </div>
-       `
-   }).join("")}`
+       `;
+		})
+		.join("")}`;
 }
 
+// Check if book is read
 
-function isRead(book){
-    if(book.isRead == true){
-        return "Read"
+function isRead(book) {
+	if (book.isRead == true) {
+		return "Read";
+	} else {
+		return "Not Read";
+	}
+}
+
+document.body.addEventListener("click",function(e){
+    if (e.target.className == "read-update"){
+        console.log(e.target.checked);
     }
-    else{
-        return "Not Read"
+
+    if(e.target.className == "delete-book"){
+       const cardIndex = e.target.parentNode.dataset.bookIndex;
+       deleteBook(cardIndex);
+       showBook()
+       deleteBookPopup()
     }
+})
+
+
+// Delete book function 
+
+function deleteBook(index){
+    bookArray.splice(index, 1)
+}
+
+// Deleted book popup 
+
+function deleteBookPopup(){
+    toaster.textContent = "Book Deleted";
+		toaster.style.backgroundColor = "#f2dede";
+		toaster.style.color = "#a94442";
+		toaster.className = "show";
+		setTimeout(function () {
+			toaster.classList.remove("show");
+		}, 2000);
 }
